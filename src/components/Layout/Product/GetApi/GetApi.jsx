9da6d5b6f9904c67439d3e9082065bc24a5d6ef3
duonghoccodefullstack <dbui99620.com>
2024-08-16 00:21:41 +0700

    feat: Connect data base with fire base

--- src/components/Layout/Product/GetApi/GetApi.jsx
+++ src/components/Layout/Product/GetApi/GetApi.jsx
@@ -1,16 +1,25 @@
 import axios from 'axios';
+import { getDatabase, ref, child, get } from "firebase/database";
 
 export const fetchMovies = async () => {
     try {
-        const response = await axios.get('/api/searchMovie.json'); 
+        const response = await axios.get('/api/searchMovie.json');
         return response.data;
     } catch (error) {
         console.error('Error fetching movies:', error);
         throw error;
     }
 };
+const dbRef = ref(getDatabase());
+export const Mobile = get(child(dbRef, `data/movies`)).then((snapshot) => {
+    return snapshot.val()
+}).catch((error) => {
+    console.error(error);
+});
 
 export const Movie = async () => {
+
+    
     try {
         const response = await axios.get('/api/movie.json'); 
         return response.data.movies;
@@ -29,18 +38,18 @@ export const convertMinutesToHhMm = (minutes) => {
     const hours = Math.floor(minutes / 60);
     const remainingMinutes = minutes % 60;
     return `${hours.toString().padStart(2)}h ${remainingMinutes.toString().padStart(2, '0')}m`;
-  };
+};
 
 export const TimestampConverter = ({ seconds, nanoseconds }) => {
     const date = new Date(seconds * 1000 + nanoseconds / 1000000);
     const formattedDate = date.toUTCString();
     return (
-      <div>
-        <h3>Timestamp:</h3>
-        <p>{`Seconds: ${seconds}`}</p>
-        <p>{`Nanoseconds: ${nanoseconds}`}</p>
-        <h4>Converted Date:</h4>
-        <p>{formattedDate}</p>
-      </div>
+        <div>
+            <h3>Timestamp:</h3>
+            <p>{`Seconds: ${seconds}`}</p>
+            <p>{`Nanoseconds: ${nanoseconds}`}</p>
+            <h4>Converted Date:</h4>
+            <p>{formattedDate}</p>
+        </div>
     );
-  };
+};
