ebffef403830c5c81f855361e14c5ebf3faaee0c
duonghoccodefullstack <dbui99620.com>
2024-12-11 03:00:53 +0700

    Add README.md with project description

--- client/src/context/Theme/index.jsx
+++ client/src/context/Theme/index.jsx
@@ -1,24 +1,4 @@
-// import { createContext,useContext,useState,useEffect } from 'react';
-// const ThemeContext = createContext()
-// export const ThemeProvider = ({ children }) => {
-//     const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
-//     const [color,setColor] = useState(localStorage.getItem('color') || 'white');
-//     useEffect(() => {
-//         localStorage.setItem('theme', theme);
-//         localStorage.setColor('color', color);
-//     }, [theme,color]);
-// // giá trị mặc định
 
-//     // const value = { theme, setTheme };
-
-//     return (
-//         <ThemeContext.Provider value={{ theme, setTheme,setColor,color }}>
-//             {children}
-//         </ThemeContext.Provider>
-//     );
-// };
-
-// export const useTheme = () => useContext(ThemeContext);
 import { createContext, useContext, useState, useEffect } from 'react';
 
 
